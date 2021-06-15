a319b1c28413b0c74f41e1c58f6b52014be0b39c
AgentO3 <ozanzal@gmail.com>
2021-06-14 21:00:54 -0400

    Fix issue when connecting with multiple instances running

--- auto.js
+++ auto.js
@@ -38,71 +38,80 @@ const argv = yargs
 var connect = (outpoint) => {
     var magic = Buffer.from("524d5654", "hex")
 
-    outpoint.transfer(magic, function(error) {
-        if(error) {
+    outpoint.transfer(magic, function (error) {
+        if (error) {
             return
         }
         console.debug("send magic bytes")
     })
 }
 
+var dataLength = 0
+
 var attach = (device) => {
-    
-    if(device.deviceDescriptor.idVendor != "0x2ca3" & device.deviceDescriptor.idProduct != "0x1f") {
+
+    if (device.deviceDescriptor.idVendor != "0x2ca3" & device.deviceDescriptor.idProduct != "0x1f") {
         return
     }
 
     var goggles = device
 
-    goggles.open()
-    if(!goggles.interfaces) {
+    try {
+        goggles.open()
+
+        if (!goggles.interfaces) {
+            console.error("Couldn't open Goggles USB device")
+            return
+        }
+        var interface = goggles.interface(3)
+        interface.claim()
+        if (!interface.endpoints) {
+            console.error("Couldn't claim bulk interface")
+            return
+        }
+
+        if (!argv.f && !argv.o) {
+            console.log("warning: no outputs specified")
+            argv.v = true
+        }
+
+        var fd
+
+        var inpoint = interface.endpoints[1]
+        inpoint.timeout = 100
+        //outpoint.timeout = 200
+
+        var outpoint = interface.endpoints[0]
+
+        connect(outpoint)
+
+        inpoint.addListener("data", function (data) {
+            dataLength = data.length
+            if (argv.o) {
+                process.stdout.write(data)
+            }
+            if (argv.v) {
+                console.debug("received " + data.length + " bytes")
+            }
+            if (argv.f) {
+                if (!fd) {
+                    fd = fs.openSync(argv.f, "w")
+                    if (!fd) {
+                        console.error("couldn't open file " + argv.f + ": " + err)
+                        process.exit(1)
+                    }
+                }
+                fs.writeSync(fd, data)
+            }
+        })
+        inpoint.addListener("error", function (error) {
+            console.error(error)
+        })
+        inpoint.startPoll(argv.q, argv.s)
+    } catch {
         console.error("Couldn't open Goggles USB device")
         return
     }
-    var interface = goggles.interface(3)
-    interface.claim()
-    if(!interface.endpoints) {
-        console.error("Couldn't claim bulk interface")
-        return
-    }
-    
-    if(!argv.f && !argv.o) {
-        console.log("warning: no outputs specified")
-        argv.v = true
-    }
-    
-    var fd
-    
-    var inpoint = interface.endpoints[1]
-    inpoint.timeout = 100
-    //outpoint.timeout = 200
-    
-    var outpoint = interface.endpoints[0]
-    
-    connect(outpoint)
-
-    inpoint.addListener("data", function(data) {
-        if(argv.o) {
-            process.stdout.write(data)
-        }
-        if(argv.v) {
-            console.debug("received "+data.length+" bytes")
-        }
-        if(argv.f) {
-            if(!fd) {
-                fd = fs.openSync(argv.f, "w")
-                if(!fd) {
-                    console.error("couldn't open file "+ argv.f + ": "+err)
-                    process.exit(1)
-                }
-            }
-            fs.writeSync(fd, data)
-        }
-    })
-    inpoint.addListener("error", function(error) {
-        console.error(error)
-    })
-    inpoint.startPoll(argv.q, argv.s)
 }
 
 usb.on('attach', (d) => {
